93af8ad0429791c1152ac5fad95ba953945dc3ba
Istiak98 <istiakahmed104127@gmail.com>
2021-12-24 20:41:48 +0600

    updated24dec2021

--- src/components/Home/Blogs/Blogs.js
+++ src/components/Home/Blogs/Blogs.js
@@ -2,11 +2,278 @@ import React from "react";
 import "./Blogs.css";
 const Blogs = () => {
   return (
-    <section class="wrapper mt-2">
-      <div class="container-fostrap">
-       <h2>New Blogs Comming Soon</h2>
+    <div>
+      {/* <header>
+        <h1 className="fw-bold fs-1">
+          Difference between React JS and React Native
+        </h1>
+      </header>
+      <article>
+        <p className="text-start">
+          React JS and React Native are two quite popular tools that are
+          utilized for programming a multitude of applications and websites.
+          What’s more interesting that each of their unique features is their
+          unknown history. Facebook, in the year 2011, realized that they need a
+          newer model with an advanced user interface to cater to the growing
+          demands of the application. Constant updates and developments required
+          the realignment of the entire application which is why, Jordon Walke,
+          an engineer at Facebook, came up with the prototype for React JS.
+          What’s more? This innovation had turned out to be a game-changer for
+          the company. Later on in the year 2015, they came out with React
+          Native which built an impressive groundwork towards developing
+          applications, for both smartphones and websites. It’s alright to have
+          your doubts when it comes to picking either one of them. In case you
+          are unable to conclude which one is better than the two, this article
+          is just for you. We have included crucial features of both, React
+          Native and React JS, which will assist you in decrypting the
+          differences, and distinguishing between React JS and React Native
+          accordingly. Before we proceed with differentiating between the two,
+          let us first understand what’s React JS and React Native.
+        </p>
+        <hr class="article-divider" />
+      </article>
+      <article>
+        <h1 className="fw-bold text-start">An overview of React JS</h1>
+        <date></date>
+        <p className="text-start">
+          React JS is an open-source JavaScript library that is used to build
+          user interfaces for applications, specifically for the web. You can
+          use a feature called ‘components’ which is a small part of a code in
+          order to compose complex user interfaces. React JS mainly incorporates
+          two segments: One of them is components which include pieces of HTML
+          code that are visible in your user interfaces and the other segment is
+          the HTML document where the rendering process of the components that
+          you use will take place.
+        </p>
+        <h1 className="fw-bold text-start">An overview of React Native</h1>
+        <p className="text-start">
+          React Native, which came after React JS, is an open-source JavaScript
+          framework. Developers use React Native to develop mobile and web
+          applications, suited for iOS, Android as well as Windows. It focuses
+          on developing cross-platform mobile applications using JavaScript
+          only. React Native and React JS are quite similar in this manner but
+          React Native makes use of native components as its building blocks.
+          Moreover, React Native is best suited for mobiles rather than the web.
+        </p>
+        <hr class="article-divider" />
+      </article>
+      <article>
+        <h1 className="fw-bold text-start">Conclusion</h1>
+        <date></date>
+        <p className="text-start">
+          React JS and React Native are a necessity for app developers. Their
+          functioning systems are constantly evolving and their flexibility is
+          well-liked by several developers. In one way or the other, both React
+          JS and React Native are optimal for certain types of applications.
+          While React JS is more of a JavaScript library, React Native is an
+          entire framework in itself. This makes React JS the origin point and
+          the heart of Native. This way, both seem to be complementing each
+          other. Also Read: A Beginner’s guide to getting started with React
+          Both frameworks have their strengths and limitations. In an
+          environment where technology is developing at a consistent pace, you
+          will be bombarded with improvements and updates which could be a
+          hassle sometimes. Nevertheless, you cannot expect everything to work
+          perfectly. All you need is an adequate amount of skill and knowledge
+          and you are good to go. The bottom line is, deciding between React JS
+          and React Native should be child’s play once you determine the kind of
+          application that you want to develop.
+        </p>
+        <hr class="article-divider" />
+      </article> */}
+
+      <div class="container overflow-hidden">
+        <div class="row gx-3 mt-5 mb-3">
+          <div class="col-sm-12 col-md-6">
+            <div class="p-3 border">
+              <header>
+                <h1 className="fw-bold fs-1">
+                  Difference between React JS and React Native
+                </h1>
+              </header>
+              <article>
+                <p className="text-start">
+                  React JS and React Native are two quite popular tools that are
+                  utilized for programming a multitude of applications and
+                  websites. What’s more interesting that each of their unique
+                  features is their unknown history. Facebook, in the year 2011,
+                  realized that they need a newer model with an advanced user
+                  interface to cater to the growing demands of the application.
+                  Constant updates and developments required the realignment of
+                  the entire application which is why, Jordon Walke, an engineer
+                  at Facebook, came up with the prototype for React JS. What’s
+                  more? This innovation had turned out to be a game-changer for
+                  the company. Later on in the year 2015, they came out with
+                  React Native which built an impressive groundwork towards
+                  developing applications, for both smartphones and websites.
+                  It’s alright to have your doubts when it comes to picking
+                  either one of them. In case you are unable to conclude which
+                  one is better than the two, this article is just for you. We
+                  have included crucial features of both, React Native and React
+                  JS, which will assist you in decrypting the differences, and
+                  distinguishing between React JS and React Native accordingly.
+                  Before we proceed with differentiating between the two, let us
+                  first understand what’s React JS and React Native.
+                </p>
+                <hr class="article-divider" />
+              </article>
+              <article>
+                <h1 className="fw-bold text-start">An overview of React JS</h1>
+                <date></date>
+                <p className="text-start">
+                  React JS is an open-source JavaScript library that is used to
+                  build user interfaces for applications, specifically for the
+                  web. You can use a feature called ‘components’ which is a
+                  small part of a code in order to compose complex user
+                  interfaces. React JS mainly incorporates two segments: One of
+                  them is components which include pieces of HTML code that are
+                  visible in your user interfaces and the other segment is the
+                  HTML document where the rendering process of the components
+                  that you use will take place.
+                </p>
+                <h1 className="fw-bold text-start">
+                  An overview of React Native
+                </h1>
+                <p className="text-start">
+                  React Native, which came after React JS, is an open-source
+                  JavaScript framework. Developers use React Native to develop
+                  mobile and web applications, suited for iOS, Android as well
+                  as Windows. It focuses on developing cross-platform mobile
+                  applications using JavaScript only. React Native and React JS
+                  are quite similar in this manner but React Native makes use of
+                  native components as its building blocks. Moreover, React
+                  Native is best suited for mobiles rather than the web.
+                </p>
+                <hr class="article-divider" />
+              </article>
+              <article>
+                <h1 className="fw-bold text-start">Conclusion</h1>
+                <date></date>
+                <p className="text-start">
+                  React JS and React Native are a necessity for app developers.
+                  Their functioning systems are constantly evolving and their
+                  flexibility is well-liked by several developers. In one way or
+                  the other, both React JS and React Native are optimal for
+                  certain types of applications. While React JS is more of a
+                  JavaScript library, React Native is an entire framework in
+                  itself. This makes React JS the origin point and the heart of
+                  Native. This way, both seem to be complementing each other.
+                  Also Read: A Beginner’s guide to getting started with React
+                  Both frameworks have their strengths and limitations. In an
+                  environment where technology is developing at a consistent
+                  pace, you will be bombarded with improvements and updates
+                  which could be a hassle sometimes. Nevertheless, you cannot
+                  expect everything to work perfectly. All you need is an
+                  adequate amount of skill and knowledge and you are good to go.
+                  The bottom line is, deciding between React JS and React Native
+                  should be child’s play once you determine the kind of
+                  application that you want to develop.
+                </p>
+                <hr class="article-divider" />
+              </article>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div class="p-3 border ">
+              <h1 className="fs-1 fw-bold">CRUD</h1>
+              <article>
+                <p className="text-start">
+                  In computer programming, Create, Read, Update, and Delete
+                  (CRUD) are the four basic functions of persistent storage.
+                  Alternative terms may be used to define the four basic
+                  functions of CRUD. Get instead of read, change instead of
+                  update, or discard instead of delete. CRUD may also be used to
+                  describe user interface rules that make it easier to view,
+                  retrieve, and modify information. Often uses computerized
+                  forms and reports. This term was probably first introduced by
+                  James Martin in his 1983 book, Managing Database Environments.
+                  You can extend the acronym to CRUDL to cover the listing of
+                  large datasets that add complexity such as: B. Pagination when
+                  the record is too large to be easily saved in memory. CRUD is
+                  an acronym for Create, Read, Update, and Delete. These are the
+                  four most basic operations that can be performed on most
+                  traditional database systems and form the backbone for
+                  interacting with any database
+                </p>
+                <ul>
+                  <li>
+                    Create operations, which involve writing data to the
+                    database
+                  </li>
+                  <li>
+                    Read operations, which query a database to retrieve data
+                    from it
+                  </li>
+                  <li>
+                    Update operations, which change data that already exists in
+                    a database
+                  </li>
+                  <li>
+                    Delete operations, which permanently remove data from a
+                    database
+                  </li>
+                </ul>
+                <hr class="article-divider" />
+              </article>
+              <article>
+                <p className="text-start">
+                  <h5 className="fs-1 fw-bold">Create</h5>
+                  <p>
+                    The first character of CRUD, `C`, refers to CREATE, also
+                    known as add, insert. This operation expects a new record to
+                    be inserted using the SQL insert statement. SQL uses the
+                    INSERT INTO statement to create a new record in the table.
+                  </p>
+                  <br />
+                  <h5 className="fs-1 fw-bold">Read</h5>
+                  <p>
+                    The second character in CRUD, `R`, indicates a SELECT
+                    operation. The word "read" retrieves data or recordsets from
+                    one or more of the listed tables. SQL uses a SELECT
+                    statement to retrieve the data. You can use SQL Server
+                    Management Studio, SQL Server Data Tools, or sqlcmd to run
+                    queries as needed.
+                  </p>
+                  <br />
+                  <h5 className="fs-1 fw-bold">Update</h5>
+                  <p>
+                    The third character in CRUD, `U`, indicates the update
+                    process. When you use the keyword Update, SQL makes changes
+                    to one or more existing data records in the table. When you
+                    perform an update, you need to define the target tables and
+                    columns that need to be updated, and their values. You may
+                    also need to know which row needs to be updated. In general,
+                    you should limit the number of rows to avoid lock escalation
+                    and parallelism issues.
+                  </p>
+                  <h5 className="fs-1 fw-bold">Delete</h5>
+                  <p>
+                    The last character in a CRUD operation is "D", which means
+                    to delete a record from the table. SQL uses the SQLDELETE
+                    command to delete a record from a table
+                  </p>
+                </p>
+              </article>
+              <article>
+                <h5 className="fs-1 fw-bold text-start">Summary</h5>
+                <p className="text-start">
+                  So far, I've talked a lot about CRUD operations. It is known
+                  as the basis for SQL operations in all database products. I
+                  also explained how to implement CRUD operations in SQL Server.
+                  We encourage you to read the articles about SQL Insert, SQL
+                  Delete, and SQL Update that are part of the CRUD operation.
+                  Implementing create, update, delete, and insert operations is
+                  relatively easy because they are very similar operations. The
+                  most efficient way to implement CRUD operations in SQL is to
+                  use stored procedures. For more information, see the article
+                  Creating and using CRUD stored procedures.
+                </p>
+                <hr class="article-divider" />
+              </article>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
